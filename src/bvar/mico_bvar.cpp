3bc647d6a319266cb255f782fe63483eaf20b05e
jiangdongzi <jiangdongzidi@126.com>
2024-02-01 21:19:41 +0800

    [update]change mico_bvar

--- src/bvar/mico_bvar.cpp
+++ src/bvar/mico_bvar.cpp
@@ -1,7 +1,8 @@
 #include <bvar/mico_bvar.h>
 #include "brpc/metric_rpc.pb.h"
+#include "bvar/multi_dimension.h"
+#include <brpc/channel.h>
 
-bthread_t bvar_stat_tid;
 class PrometheusDumper : public bvar::Dumper {
 public:
     bool dump(const std::string& name,
@@ -9,9 +10,13 @@ public:
         auto* new_metric = req.add_metric();
         new_metric->set_key(name);
         new_metric->set_value(description.as_string());
+        if (req.metric_size() >= 100) {
+          reqs.emplace_back(std::move(req));
+        }
         return true;
     }
     brpc_metrics::MetricRequest req;
+    std::vector<brpc_metrics::MetricRequest> reqs;
 };
 
 static brpc::Channel& GetPrometheusChannel (const std::string& pushgateway_server) {
@@ -39,29 +44,41 @@ static void* dump_bvar(void* arg) {
     }
     bvar::DumpOptions opts;
     bvar::MVariable::dump_exposed(&d, &opts);
-    if (d.req.metric_size() == 0) continue;
+    std::vector<brpc_metrics::MetricRequest> reqs;
+    reqs.swap(d.reqs);
+    if (d.req.metric_size() > 0) {
+      reqs.emplace_back(std::move(d.req));
+    }
+    if (d.reqs.empty()) continue;
     static brpc::Channel& channel = GetPrometheusChannel(*pushgateway_server_ptr);
-    brpc_metrics::MetricRequest req;
     brpc_metrics::MetricResponse rsp;
-    req.Swap(&d.req);
     brpc_metrics::MetricService_Stub stub(&channel);
-    brpc::Controller cntl;
-    stub.CollectMetrics(&cntl, &req, &rsp, nullptr);
-    if (cntl.Failed()) {
-        LOG(ERROR) << "dump bvar failed, error_code: " << cntl.ErrorCode() << ", error_text: " << cntl.ErrorText();
-    } else {
-        LOG(INFO) << "dump bvar to prometheus, metric size: " << req.metric_size();
+    for (const auto& ele : reqs) {
+      brpc::Controller cntl;
+      stub.CollectMetrics(&cntl, &ele, &rsp, nullptr);
+      if (cntl.Failed()) {
+          LOG(ERROR) << "dump bvar failed, error_code: " << cntl.ErrorCode() << ", error_text: " << cntl.ErrorText();
+      } else {
+          LOG(INFO) << "dump bvar to prometheus, metric size: " << ele.metric_size();
+      }
+      rsp.Clear();
     }
   }
 
   return nullptr;
 }
 
-void start_stat_bvar(const std::string& pushgateway_server) {
-    google::SetCommandLineOption("bvar_max_dump_multi_dimension_metric_number", "1000");
+static void start_stat_bvar_internal(const std::string& pushgateway_server) {
+    google::SetCommandLineOption("bvar_max_dump_multi_dimension_metric_number", "10000");
     google::SetCommandLineOption("bvar_dump_interval", "180");
     std::unique_ptr<std::string> pushgateway_server_ptr(new std::string(pushgateway_server));
+    bthread_t bvar_stat_tid;
     bthread_start_background(&bvar_stat_tid, nullptr, dump_bvar, pushgateway_server_ptr.release());
+}
+
+void start_stat_bvar(const std::string& pushgateway_server) {
+  static std::once_flag flag;
+  std::call_once(flag, start_stat_bvar_internal, pushgateway_server);
 }
 
 std::string brpc_get_host_name(){
@@ -80,4 +97,48 @@ std::string brpc_get_app_name(){
     std::string exec_name = path;
     std::string app_name = exec_name.substr(exec_name.find_last_of('/') + 1);
     return app_name;
+}
+
+static thread_local butil::FlatMap<std::string, bvar::LatencyRecorder*> tls_latency_recorder;
+static butil::FlatMap<std::string, bvar::LatencyRecorder*> g_latency_recorder;
+static std::string app_name;
+static std::string host_name;
+static std::list<std::string> svr_identity;
+static std::list<std::string> svr_identity_label_name {"app_name", "host_name"};
+class LatencyRecorderInitHelper {
+public:
+    LatencyRecorderInitHelper() {
+        g_latency_recorder.init(512);
+        app_name = brpc_get_app_name();
+        host_name = brpc_get_host_name();
+        svr_identity = {app_name, host_name};
+    }
+};
+static LatencyRecorderInitHelper latencyrecorderinithelper;
+
+class TLSLatencyRecorderInitHelper {
+public:
+    TLSLatencyRecorderInitHelper() {
+        tls_latency_recorder.init(512);
+    }
+};
+
+static thread_local TLSLatencyRecorderInitHelper tlslatencyrecorderinithelper;
+
+static std::vector<std::unique_ptr<bvar::MultiDimension<bvar::LatencyRecorder>>> mul_latency_recorder_holder;
+bvar::LatencyRecorder& get_latency_recorder(const std::string& metric_name) {
+    auto* valptr = tls_latency_recorder.seek(metric_name);
+    if (valptr != nullptr) {
+        return *(*valptr);
+    }
+    static std::mutex latency_recorder_mt;
+    std::lock_guard<std::mutex> lock(latency_recorder_mt);
+    valptr = g_latency_recorder.seek(metric_name);
+    if (valptr == nullptr) {
+        mul_latency_recorder_holder.emplace_back(new bvar::MultiDimension<bvar::LatencyRecorder>(metric_name, svr_identity_label_name));
+        g_latency_recorder[metric_name] = mul_latency_recorder_holder.back()->get_stats(svr_identity);
+        valptr = g_latency_recorder.seek(metric_name);
+    }
+    tls_latency_recorder[metric_name] = *valptr;
+    return *(*valptr);
 }
