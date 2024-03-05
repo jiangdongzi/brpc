0f8b883167d39c9fcc385642f873a26b3823831e
jiangdongzi <jiangdongzidi@126.com>
2024-03-05 16:37:58 +0800

    fix thread_local bvar concurrency

--- src/bvar/mico_bvar.h
+++ src/bvar/mico_bvar.h
@@ -9,20 +9,20 @@ bvar::CountRecorder& get_count_recorder (const std::string& metric_name);
 bvar::WindowEx<bvar::IntRecorder, 20>& get_win_mean_recorder (const std::string& metric_name);
 
 class LatencyRecorderGuard {
-    bvar::LatencyRecorder* _recorder;
+    std::string _metric_name;
     butil::Timer _timer;
     public:
-    LatencyRecorderGuard(bvar::LatencyRecorder* recorder) : _recorder(recorder) {
+    LatencyRecorderGuard(const std::string& metric_name) : _metric_name(metric_name) {
         _timer.start();
     }
     ~LatencyRecorderGuard() {
         _timer.stop();
-        (*_recorder) << _timer.u_elapsed();
+        get_latency_recorder(_metric_name) << _timer.u_elapsed();
     }
 };
 
 #define SCOPED_LATENCY_RECORDER(metric_name) \
-    LatencyRecorderGuard BAIDU_CONCAT(scoped_latecy_recorder_dummy_at_line_, __LINE__)(&get_latency_recorder(metric_name))
+    LatencyRecorderGuard BAIDU_CONCAT(scoped_latecy_recorder_dummy_at_line_, __LINE__)(metric_name)
 
 inline void SetCountRecorder(const std::string& metric_name, const int64_t count = 1) {
     get_count_recorder(metric_name) << count;
