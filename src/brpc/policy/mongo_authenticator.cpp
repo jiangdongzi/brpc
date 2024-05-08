6a120c1bfd91b0ea12df2d31149e77c58c29ddd8
jiangdongzi <jiangdongzidi@126.com>
2024-05-08 10:01:47 +0800

    stash

--- src/brpc/policy/mongo_authenticator.cpp
+++ src/brpc/policy/mongo_authenticator.cpp
@@ -17,6 +17,12 @@
 
 #include "brpc/policy/mongo_authenticator.h"
 
+#include <openssl/rand.h>
+#include <openssl/evp.h>
+#include <openssl/md5.h>
+#include <openssl/bio.h>
+#include <openssl/hmac.h>
+#include <openssl/buffer.h>
 #include "butil/base64.h"
 #include "butil/iobuf.h"
 #include "butil/string_printf.h"
@@ -24,6 +30,9 @@
 #include "brpc/redis_command.h"
 #include <bsoncxx/builder/basic/document.hpp>
 #include <bsoncxx/types.hpp>
+#include "butil/base64.h"
+#include "butil/sha1.h"
+#include "butil/fast_rand.h"
 #include "brpc/policy/mongo.pb.h"
 #include <brpc/channel.h>
 #include <bsoncxx/builder/stream/document.hpp>
@@ -31,77 +40,50 @@
 namespace brpc {
 namespace policy {
 
-static // 生成客户端随机数的函数
-void generate_client_nonce(char *nonce, size_t size) {
-    const char *chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    for (size_t i = 0; i < size; i++) {
-        int index = rand() % (strlen(chars));
-        nonce[i] = chars[index];
+static std::string generate_client_nonce() {
+    std::string nonce;
+    nonce.resize(24);
+    for (int i = 0; i < 24; i++) {
+        nonce[i] = butil::fast_rand_less_than(256);
     }
-    nonce[size] = '\0';  // 字符串结束符
+    return nonce;
+}
+
+static std::string HMAC_SHA1(const std::string& key, const std::string& data) {
+    unsigned int len = 0;
+    unsigned char digest[20];
+    HMAC(EVP_sha1(), key.c_str(), key.size(), (unsigned char*)data.c_str(), data.size(), digest, &len);
+    std::string result((char*)digest, len);
+    return result;
+}
+
+std::string SCRAM_salt_password(const std::string& password,
+                                 const std::string& salt,
+                                 int iterations) {
+    std::string start_key = salt + std::string("\x00\x00\x00\x01", 4);
+    std::string intermediate_digest;
+    std::string output = intermediate_digest = HMAC_SHA1(password, start_key);
+    for (int i = 2; i <= iterations; i++) {
+        intermediate_digest = HMAC_SHA1(password, intermediate_digest);
+        for (int k = 0; k < 20; k++) {
+            output[k] ^= intermediate_digest[k];
+        }
+    }
+    return output;
+}
+
+static void AppendBinary(bsoncxx::builder::basic::document& builder,
+                         const std::string& key,
+                         const std::string& value) {
+    builder.append(bsoncxx::builder::basic::kvp(key, bsoncxx::types::b_binary{
+        bsoncxx::binary_sub_type::k_binary,
+        (uint32_t)value.size(),
+        reinterpret_cast<const uint8_t*>(value.c_str())
+    }));
 }
 
 
 int MongoAuthenticator::GenerateCredential(std::string* auth_str) const {
-    // butil::IOBuf buf;
-    // if (!passwd_.empty()) {
-    //     brpc::RedisCommandFormat(&buf, "AUTH %s", passwd_.c_str());
-    // }
-    // if (db_ >= 0) {
-    //     brpc::RedisCommandFormat(&buf, "SELECT %d", db_);
-    // }
-    // *auth_str = buf.to_string();
-    char client_nonce[24];
-    generate_client_nonce(client_nonce, 23);  // 生成一个长度为 23 的随机数
-    char first_message[128];
-    snprintf(first_message, sizeof(first_message), "n,,n=myUser,r=%s", client_nonce);
-
-    bsoncxx::builder::basic::document command;
-    command.append(bsoncxx::builder::basic::kvp("saslStart", 1));
-    command.append(bsoncxx::builder::basic::kvp("mechanism", "SCRAM-SHA-1"));
-    command.append(bsoncxx::builder::basic::kvp("payload", bsoncxx::types::b_binary{
-        bsoncxx::binary_sub_type::k_binary,
-        (uint32_t)strlen(first_message),
-        reinterpret_cast<const uint8_t*>(first_message)
-    }));
-    command.append(bsoncxx::builder::basic::kvp("autoAuthorize", 1));
-
-    // 将 BSON 文档转换为 bson_t*
-    bsoncxx::document::view_or_value view = command.view();
-    char fullnName[256];
-    snprintf(fullnName, sizeof(fullnName), "%s.%s", "myDatabase", "$cmd");
-
-    brpc::policy::MongoRequest request;
-    brpc::policy::MongoResponse response;
-    brpc::Controller cntl;
-        brpc::Channel channel;
-    
-    // Initialize the channel, NULL means using default options. 
-    brpc::ChannelOptions options;
-    options.protocol = brpc::PROTOCOL_MONGO;
-
-    if (channel.Init("0.0.0.0:7017", "", &options) != 0) {
-        LOG(ERROR) << "Fail to initialize channel";
-        return -1;
-    }
-
-    char fullCollectionName[256];
-    snprintf(fullnName, sizeof(fullnName), "%s.%s", "myDatabase", "$cmd");
-    int32_t fullCollectionNameLen = strlen(fullCollectionName) + 1;
-    int32_t flags = 0; // No special options
-    int32_t numberToSkip = 0;
-    int32_t numberToReturn = 11; // Return all matching documents
-    request.set_full_collection_name(fullCollectionName, fullCollectionNameLen);
-    request.set_number_to_return(numberToReturn);
-    // bsoncxx::builder::stream::document document{};
-    auto v = command.view();
-    request.set_message((char*)v.data(), v.length());
-    request.mutable_header()->set_op_code(brpc::policy::DB_QUERY);
-    channel.CallMethod(NULL, &cntl, &request, &response, NULL);
-    if (cntl.Failed()) {
-        LOG(ERROR) << "Fail to access memcache, " << cntl.ErrorText();
-        return -1;
-    }
 
     return 0;
 }
