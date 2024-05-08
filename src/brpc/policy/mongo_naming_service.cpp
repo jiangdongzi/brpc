fb2d4adfc632cb2dfc2617a2252ce8b999c5dc25
jiangdongzi <jiangdongzidi@126.com>
2024-05-08 20:36:41 +0800

    stash

--- src/brpc/policy/mongo_naming_service.cpp
+++ src/brpc/policy/mongo_naming_service.cpp
@@ -29,8 +29,11 @@
 #include <bsoncxx/builder/basic/document.hpp>
 #include <memory>
 #include <netdb.h>  // gethostbyname_r
+#include <queue>
 #include <stdlib.h> // strtol
 #include <string>   // std::string
+#include <unordered_set>
+#include <vector>
 
 namespace brpc {
 namespace policy {
@@ -74,38 +77,67 @@ static std::string GetIsMasterMsg (const std::string mongo_uri_str, const std::s
 
 int MongoNamingService::GetServers(const char *uri, std::vector<ServerNode> *servers) {
     servers->clear();
-
-    const butil::MongoDBUri mongo_uri = butil::parse_mongo_uri(uri);
-
-    brpc::RedisRequest request;
-    if (!request.AddCommand("CLUSTER SLOTS")) {
-        LOG(ERROR) << "Fail to add command";
-        return -1;
-    }
-
-    brpc::RedisResponse response;
-    brpc::Controller cntl;
-    // channel.CallMethod(NULL, &cntl, &request, &response, NULL);
-    // if (cntl.Failed()) {
-    //     LOG(ERROR) << "Fail to access redis, " << cntl.ErrorText();
-    //     return -1;
-    // }
-    const auto& reply = response.reply(0);
-    for (int i = 0; i < reply.size(); i++) {
-        const auto& slot_start = reply[i][0];
-        const auto& slot_end = reply[i][1];
-        const std::string tag = std::to_string(slot_start.integer()) + "-" + std::to_string(slot_end.integer());
-
-        const auto& ip = reply[i][2][0];
-        const auto& port = reply[i][2][1];
-        butil::EndPoint point;
-        if (butil::str2endpoint(ip.c_str(), port.integer(), &point) != 0 &&
-            butil::hostname2endpoint(ip.c_str(), port.integer(), &point) != 0) {
-            LOG(ERROR) << "Invalid address=`" << ip.c_str() << ":" << port.integer() << '\'';
+    butil::MongoDBUri mongo_uri = butil::parse_mongo_uri(uri);
+    std::vector<std::string> hosts;
+    hosts.swap(mongo_uri.hosts);
+    std::unordered_set<std::string> visited_hosts;
+    while (!hosts.empty()) {
+        const std::string& host = hosts.back();
+        hosts.pop_back();
+        if (visited_hosts.find(host) != visited_hosts.end()) {
             continue;
         }
-        servers->emplace_back(point, tag);
+        visited_hosts.insert(host);
+        std::string is_master_msg = GetIsMasterMsg(uri, host);
+        if (is_master_msg.empty()) {
+            continue;
+        }
+        // bsoncxx::document::view_or_value view = bsoncxx::from_json(is_master_msg);
+        // bsoncxx::document::element ismaster = view["ismaster"];
+        // if (ismaster.type() == bsoncxx::type::k_bool && ismaster.get_bool().value) {
+        //     servers->emplace_back(host, "master");
+        // }
+        // bsoncxx::document::element hosts_element = view["hosts"];
+        const uint8_t* data = reinterpret_cast<const uint8_t*>(is_master_msg.c_str());
+        uint32_t doc_length = *reinterpret_cast<const uint32_t*>(data);
+        if (doc_length != is_master_msg.size()) {
+            LOG(ERROR) << "Invalid BSON message length: " << doc_length;
+            continue;
+        }
+        // 创建 BSON 视图
+        bsoncxx::document::view view(data, doc_length);
+        auto v = view["ismaster"];
+        if (v.type() == bsoncxx::type::k_bool && v.get_bool().value) {
+            servers->emplace_back(host, "master");
+        } else {
+            servers->emplace_back(host, "slave");
+        }
+        auto it = view.find("hosts");
+        if (it != view.end()) {
+            auto hosts_arr = it->get_array().value;
+            for (auto host : hosts_arr) {
+                hosts.emplace_back(host.get_string().value.to_string());
+            }
+        }
     }
+
+
+    // const auto& reply = response.reply(0);
+    // for (int i = 0; i < reply.size(); i++) {
+    //     const auto& slot_start = reply[i][0];
+    //     const auto& slot_end = reply[i][1];
+    //     const std::string tag = std::to_string(slot_start.integer()) + "-" + std::to_string(slot_end.integer());
+
+    //     const auto& ip = reply[i][2][0];
+    //     const auto& port = reply[i][2][1];
+    //     butil::EndPoint point;
+    //     if (butil::str2endpoint(ip.c_str(), port.integer(), &point) != 0 &&
+    //         butil::hostname2endpoint(ip.c_str(), port.integer(), &point) != 0) {
+    //         LOG(ERROR) << "Invalid address=`" << ip.c_str() << ":" << port.integer() << '\'';
+    //         continue;
+    //     }
+    //     servers->emplace_back(point, tag);
+    // }
     return 0;
 }
 
