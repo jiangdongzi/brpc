da5bc57c55e14393c3815d1053dfda9a3554be68
jiangdongzi <jiangdongzidi@126.com>
2024-05-10 15:58:39 +0800

    stash

--- src/butil/mongo_utils.h
+++ src/butil/mongo_utils.h
@@ -36,16 +36,47 @@ std::string SerializeBsonDocView(const bsoncxx::builder::basic::document& doc);
 std::vector<bsoncxx::document::view> DeSerializeBsonDocView(const std::string& str);
 
 namespace mongo {
+class Client;
+class Database;
+
+class Collection {
+public:
+    std::string name;
+    Collection(const std::string& collection_name, Database* const db) {
+        name = collection_name;
+        database = db;
+    }
+    Database* database;
+};
+
+class Database {
+public:
+    std::string name;
+    Database(const std::string& db_name, Client* c) {
+        name = db_name;
+        client = c;
+    }
+    Collection operator[](const std::string& collection_name) {
+        return Collection(collection_name, this);
+    }
+    Client* client;
+};
+
 class Client {
 public:
     Client(const std::string& mongo_uri);
+    Database operator[](const std::string& database_name) {
+        return Database(database_name, this);
+    }
 
+brpc::Channel* channel;
 private:
-    brpc::Channel* channel;
     static std::unordered_map<std::string, std::unique_ptr<brpc::Channel>> channels;
     static thread_local std::unordered_map<std::string, brpc::Channel*> tls_channels;
     static brpc::Channel* GetChannel(const std::string& mongo_uri);
 };
+
+
 } // namespace mongo
 
 
