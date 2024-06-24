343873e73548c145bad4c854ad18a13957f2b068
jiangdongzi <jiangdongzidi@126.com>
2024-06-24 20:10:22 +0800

    [log]SetPossibleGoAwayStreamId

--- src/brpc/policy/http2_rpc_protocol.cpp
+++ src/brpc/policy/http2_rpc_protocol.cpp
@@ -1846,7 +1846,7 @@ void PackH2Request(butil::IOBuf*,
 }
 
 static bool IsH2SocketValid(Socket* s, Socket* raw_socket) {
-    LOG(INFO) << "last_sent_stream_id=" << s->last_sent_stream_id.load(butil::memory_order_relaxed) << ", possible_h2_max_stream_id=" << raw_socket->possible_h2_max_stream_id;
+    LOG(INFO) << "id" << s->id() << ", last_sent_stream_id=" << s->last_sent_stream_id.load(butil::memory_order_relaxed) << ", raw_id: " << raw_socket->id() << ", possible_h2_max_stream_id=" << raw_socket->possible_h2_max_stream_id;
     return s->last_sent_stream_id.fetch_add(2, butil::memory_order_relaxed) <= raw_socket->possible_h2_max_stream_id;
 }
 
