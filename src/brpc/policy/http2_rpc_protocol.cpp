f54e9dbcff4df442f34f2fe06f296c43ce123bde
jiangdongzi <jiangdongzidi@126.com>
2024-07-08 18:01:52 +0800

    [log]

--- src/brpc/policy/http2_rpc_protocol.cpp
+++ src/brpc/policy/http2_rpc_protocol.cpp
@@ -410,7 +410,7 @@ void H2Context::RemoveGoAwayStreams(
              it != _pending_streams.end(); ++it) {
             if (it->first > goaway_stream_id) {
                 //打印socketid, streamid
-                LOG(INFO) << "RemoveGoAwayStreams: " << _socket->id() << " " << it->first << " socket: " << (void*)it->second->socket();
+                LOG(INFO) << "RemoveGoAwayStreams: " << _socket->id() << " " << it->first << " socket: " << (void*)it->second->socket() << " msg: " << (void*)it->second << " co_id: " << it->second->correlation_id();
                 out_streams->push_back(it->second);
                 _socket->CancelNotify(bthread_id_t{it->second->correlation_id()});
             }
